de700865a749a1549e749ca037f77b0556432dc5
Scott Hellings <scott.hellings@netmatters-scs.com>
2021-09-08 11:28:26 +0100

    added slider js and content

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,42 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
-
-const src = {
-  styles : ['./scss/**/*.scss']
-}
+const babel = require('gulp-babel')
 
 const  paths = {
   styles: {
-    src: './scss/**/*.scss',
+    src: './src/scss/**/*.scss',
     dest: './css/'
+  },
+  scripts : {
+    src: './src/js/app.js',
+    dest: './js/'
   }
 }
+
 function styles() {
   return gulp.src(paths.styles.src)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(paths.styles.dest))
 }
 
+function scripts() {
+  return gulp.src(paths.scripts.src)
+    .pipe(babel({
+      presets: ['@babel/preset-env']
+    }))
+    .pipe(gulp.dest(paths.scripts.dest))
+}
+
 function watch() {
   gulp.watch(paths.styles.src, styles)
+  gulp.watch(paths.scripts.src, scripts)
 }
 
 function run() {
-  return gulp.series( styles, watch)()
+  return gulp.series( styles, scripts, watch)()
 }
 
 exports.styles = styles
+exports.scripts = scripts
 exports.watch = watch
 exports.default = run
