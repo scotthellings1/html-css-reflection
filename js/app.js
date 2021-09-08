03b6ec4ae506fb083fa45f2728a86794afb97e30
Scott Hellings <scott.hellings@netmatters-scs.com>
2021-09-08 17:02:59 +0100

    fixed image ling=ks in slider

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var modal = document.querySelector('.cookie-container');
 var body = document.querySelector('body');
 
 if (localStorage.getItem('cookie') === null) {
-  modal.style.display = 'flex';
+  modal.style.display = 'block';
   document.querySelector('#accept-cookies').addEventListener('click', function () {
     localStorage.setItem('cookie', 'accepted');
     modal.style.display = 'none';
@@ -14,3 +14,23 @@ if (localStorage.getItem('cookie') === null) {
   modal.style.display = 'none';
   body.classList.add('default');
 }
+
+let lastScroll = 0;
+window.addEventListener("scroll", function () {
+  let currentScroll = window.pageYOffset;
+  
+  if (currentScroll <= 0) {
+    body.classList.remove("scroll-up");
+    return;
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-up");
+    body.classList.add("scroll-down");
+  } else if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-down");
+    body.classList.add("scroll-up");
+  }
+
+  lastScroll = currentScroll;
+});
--- src/js/app.js
+++ src/js/app.js
@@ -11,3 +11,25 @@ if (localStorage.getItem('cookie') === null) {
   modal.style.display = 'none';
   body.classList.add('default')
 }
+
+let lastScroll = 0;
+
+window.addEventListener("scroll", function()  {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    body.classList.remove("scroll-up");
+    return;
+  }
+  
+  if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-up");
+    body.classList.add("scroll-down");
+  } else if (
+    currentScroll < lastScroll &&
+    body.classList.contains("scroll-down")
+  ) {
+    body.classList.remove("scroll-down");
+    body.classList.add("scroll-up");
+  }
+  lastScroll = currentScroll;
+});
