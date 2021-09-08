465a82232531f113a4f36291bac9573690a7176e
Scott Hellings <scott.hellings@netmatters-scs.com>
2021-09-08 14:52:33 +0100

    ie positioning fix cookie modal

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var modal = document.querySelector('.cookie-container');
 var body = document.querySelector('body');
 
 if (localStorage.getItem('cookie') === null) {
-  modal.style.display = 'block';
+  modal.style.display = 'flex';
   document.querySelector('#accept-cookies').addEventListener('click', function () {
     localStorage.setItem('cookie', 'accepted');
     modal.style.display = 'none';
